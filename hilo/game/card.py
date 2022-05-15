f221032bd59ea9491f0e4f9b93d027c148fd134d
dsigler96 <94070628+dsigler96@users.noreply.github.com>
2022-05-14 18:05:19 -0600

    comment edits for requirements

--- hilo/game/card.py
+++ hilo/game/card.py
@@ -1,19 +1,30 @@
 import random
 
 class Card:
+    """
+    Created a class for flipping cards
+    """
 
     def __init__(self):
+        """
+        Allocated card variables
+        """
         self.score = 0
         self.card1 = 0
         self.card2 = 0
 
 
     def flip1():
+        """
+        The first flip
+        """
         Card.card1 = random.randint(1,13)
         return Card.card1
 
     def flip2(card, hi_lo):
-
+        """
+        The second flip that compares the two values of the cards and assigns points
+        """
         Card.card2 = random.randint(1,13)
         print(f"The next card is {Card.card2}")
 
